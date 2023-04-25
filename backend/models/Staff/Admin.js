f4c0b3951e0ece24e750fbe68ae4b34dd5ce9933
stephenchad <stephethanchad@gmail.com>
2023-04-25 07:53:12 +0100

    ismcellglobalcellministry model

---
+++ backend/models/Staff/Admin.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const adminSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: "admin",
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Admin = mongoose.model("Admin", adminSchema);
+module.exports = Admin;
