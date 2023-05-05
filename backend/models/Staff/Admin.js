226c54d77b1b3927f20deab0118e0b289db51769
odionjulius7 <odionjulius7@gmail.com>
2023-05-05 08:08:21 +0100

    new featured branch

--- backend/models/Staff/Admin.js
+++ backend/models/Staff/Admin.js
@@ -1,26 +1,28 @@
 const mongoose = require("mongoose");
+
+// great one
 const adminSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        role: {
-            type: String,
-            default: "admin",
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "admin",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Admin = mongoose.model("Admin", adminSchema);
